819034c890aab034f0676a0e7e756e644d701b51
Keith <Karani.keith@student.greenriver.com>
2025-11-26 12:33:58 -0800

    Added some changes to the javascript to assign functions

--- potato.js
+++ potato.js
@@ -1,1 +1,16 @@
 // Add your javascript in this file
+function potScheme () {
+    console.log("Potato scheme called");
+    document.getElementById("heading").classList.toggle("potatoScheme");
+}
+
+function potatoFact () {
+    console.log("Potato fact called");
+    document.getElementById("faq-answer").classList.toggle("hidden");
+
+}
+
+function sizeText () {
+    console.log("Initiated text size change")
+    document.body.classList.toggle("textSZ");
+}
